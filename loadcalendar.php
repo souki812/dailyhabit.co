8e020e5b996d894ecddb6f6e142556bf7beee503
Souki Alami <salam13@stlawu.edu>
2016-09-21 16:07:45 -0400

    Testing Ajax call to loadcalendar.php

--- loadcalendar.php
+++ loadcalendar.php
@@ -16,18 +16,18 @@ $user = new User($db);
       
 $id = $_SESSION['user_id'];      
 
-echo $id;
+
 
  if (!isset($db)) {
        exit();
  }else{
  		
- 		echo "loadcalendar.php";
+ 		
       //Getting all the dates clicked from the database in array format
 		$dates = $user->getdate($id);
 
 
-		echo json_encode($dates); 
+		echo $dates;
  }
 
 
