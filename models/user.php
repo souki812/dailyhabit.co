1df49d7b446ab651058a07c8cddd1519ceaa2b71
Souki Alami <salam13@stlawu.edu>
2016-09-29 21:11:36 -0400

    Testing fetchcolumn PDO

--- models/user.php
+++ models/user.php
@@ -69,7 +69,7 @@ class User {
        $select = $this->db->query("select * from dates where user_id= '$id' ");
        
 
-       $row = $select->fetchAll(PDO::FETCH_ASSOC);
+       $row = $select->fetchColumn(1);
       
 	   print_r($row);
 	   return $row;
