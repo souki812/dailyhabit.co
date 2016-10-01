e65f1bcc7e23c1b0cbf1804215705aa9c5276158
Souki Alami <salam13@stlawu.edu>
2016-10-01 18:06:07 -0400

    Testing ajax call

--- views/js/footer.js
+++ views/js/footer.js
@@ -8,7 +8,7 @@
                          data: { dbdates:dbdates },
                          success: function(response) {
                               dbdates = response;
-                              alert(dbDates);
+                              alert(dbdates);
                                           
                               }
                                  
