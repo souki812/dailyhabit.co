7396077da3cf8557ffbb9fd74b6b574c2242d87d
Souki Alami <salam13@stlawu.edu>
2016-10-05 18:44:10 -0400

    Testing the events feature of fullcalendar

--- views/js/footer.js
+++ views/js/footer.js
@@ -104,7 +104,21 @@
 
                                           }
                                             
-                        }, 
+                        },
+
+                        events: [
+                              {
+                           title: 'My Event',
+                           start: '2016-10-05',
+                           description: 'This is a cool event'
+                           }
+        // more events here
+                        ],
+                        eventRender: function(event, element) {
+                           element.qtip({
+                              content: event.description
+                        });
+                     } 
 
                         
                   }); 
