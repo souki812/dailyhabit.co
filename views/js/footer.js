e1d68831ccdd90052d672b1e5e03e61d396e13cf
Souki Alami <salam13@stlawu.edu>
2016-11-03 12:13:53 -0400

    Testing countdays method

--- views/js/footer.js
+++ views/js/footer.js
@@ -22,7 +22,6 @@
                   data: { val:val },
                   success: function(response) {
                         val = response;
-                        alert(val);
                         $('.progress-bar').css('width', val+'%').attr('aria-valuenow', val);
 
                          if (val == 100) {
@@ -65,6 +64,25 @@
                               var moment = date.format('YYYY-MM-DD');
                               
                               $(this).css('background-color', 'green');
+
+                              $.ajax({
+                                 type: 'POST',
+                                 url: 'getprogress.php',
+                                 data: { val:val },
+                                 success: function(response) {
+                                 val = response;
+                                    $('.progress-bar').css('width', val+'%').attr('aria-valuenow', val);
+
+                              if (val == 100) {
+                                 window.alert("Congratulations on completing your habit!");
+                                    }
+        
+                        // If the progress bar reaches 100% reset it back to 0
+                        if (val > 100) {
+                              val = 0;
+                           }
+                  }
+            });
                                
                                $.ajax({
                                     type: 'POST',
@@ -89,71 +107,6 @@
                                             
                         },
 
-/*
-                        //Allows a user to highlight multiple days or timeslots by clicking and dragging.
-                        selectable: true,
-
-                        //A value of true will draw a "placeholder" event while the user is dragging
-                        selectHelper: true,
-            /*
-               when user select timeslot this option code will execute.
-               It has three arguments. Start,end and allDay.
-               Start means starting time of event.
-               End means ending time of event.
-               allDay means if events is for entire day or not.
-            */
-           /*             select: function(start, end, allDay){
-               /*
-                  after selection user will be promted for enter title for event.
-               
-               var title = prompt('Event Title:');
-               /*
-                  if title is enterd calendar will add title and event into fullCalendar.
-               
-               if (title){
-
-                   var date = start;
-                              
-                                
-                               $.ajax({
-                                    type: 'POST',
-                                    url: 'AddEvent.php',
-                                    data: { date:date, title:title },
-                                    success: function(response) {
-                                          date = response;
-                                               
-                                    }
-                              });
-                  calendar.fullCalendar('renderEvent',
-                     {
-
-                        title: title,
-                        start: start,
-                        end: end,
-                        allDay: allDay
-                     },
-                     true // make the event "stick"
-                  );
-               }
-               calendar.fullCalendar('unselect');
-            },
-
-
-
-            /*
-               editable: true allow user to edit events.
-            
-            editable: true,
-            /*
-               events is the main option for calendar.
-               for demo we have added predefined events in json object.
-            
-            events: [
-               {
-                  title: 'All Day Event',
-                  start: new Date('2016-10-05')
-               },
-              ] */
 
                         
                   }); 
