9011fae099fee3ec62676a7372223796849d724d
Souki Alami <salam13@stlawu.edu>
2016-11-16 19:16:24 -0500

    Editing habits.php

---
+++ views/habits.php
@@ -0,0 +1,38 @@
+   <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DailyHabit</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="views/css/goals.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+              
+            </div>
+
+     
+<?php require('menu.php'); ?>
+
+
+    
+    
+    <div class="container PageContainer" id="secondcontainer">
+        <div class="row margintop" class="centertext">
+            
+            
+        <!--Current Goal-->
+          <div class="col-md-4 comment">
+            <div class="form-group type1">
+                <label><h3 class="form-group words"><span class="glyphicon glyphicon-road goals"></span> Current Goal:</h3></label>
+                <button  class="btn btn-default secondbutton" data-toggle="modal" data-target="#modal3" >Edit Goal</button><br>
+            </div>
+            
+           
+                
+                
+        </div>
+    </div> 
+  </div>
