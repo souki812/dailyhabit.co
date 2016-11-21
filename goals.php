451a0f415eaddf642a774f3b356b7c6fd6372db3
Souki Alami <salam13@stlawu.edu>
2016-11-21 14:05:56 -0500

    Switched habits tab with goals tab

--- goals.php
+++ goals.php
@@ -24,42 +24,9 @@ $id = $_SESSION['user_id'];
       $user = new User($db);
         
         
-        if (isset($_POST['task'])) {
-    
-                // Add a goal 
-                if ($_POST['task'] == 'current') {
-                    $success = $user->current(  $_POST['current_habit'], $_POST['current_days'], $id);
-                }
-        
-                if ($_POST['task'] == 'future') {
-                    $success = $user->future(  $_POST['future'], $id);
-                }
-        
-                if ($_POST['task'] == 'achieved') {
-                    $success = $user->achieved(  $_POST['achieved'], $id);
-                }
-        }
-    
-        // Remove a goal
-        if (isset($_POST['current_id'])) {
-            $success= $user->remove_current( $_POST['current_id']);
-        }
-
-        if (isset($_POST['future_id'])) {
-            $success= $user->remove_future( $_POST['future_id']);
-        }
-
-        if (isset($_POST['achieved_id'])) {
-            $success= $user->remove_achieved( $_POST['achieved_id']);
-        }
-
-    }
-    
-$current =  $user->selectCurrent( $id);
-$future =  $user->selectFuture( $id);
-$achieved =  $user->selectAchieved( $id);
-
+       }
 // Show whatever this activity is
 
 require('views/goals.php');
 require('views/footer.php');
+
