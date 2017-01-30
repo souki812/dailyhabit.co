5270c9f925af1aa0560f58419bd666f564d0d999
Souki Alami <salam13@stlawu.edu>
2017-01-30 15:31:29 -0500

    Testing chartjs

--- views/goals.php
+++ views/goals.php
@@ -6,7 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>DailyHabit</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-       
+            <script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.js'></script>
+
        <link rel="stylesheet" href="views/css/goals.css">
     </head>
     <body>
