0b515516f3420bac230dd6b1b3d9501b426fca7c
Souki Alami <salam13@stlawu.edu>
2017-02-02 19:10:38 -0500

    Changing the location of goal

--- goals.php
+++ goals.php
@@ -28,6 +28,8 @@ $id = $_SESSION['user_id'];
                 // Add a goal 
                 if ($_POST['task'] == 'goal') {
                     $success = $user->insert_goal( $_POST['goal'], $_POST['variable'], $_POST['unit'], $_POST['value'],  $id);
+                    $goal = $user->getgoal($id);
+                    echo $goal;
                 }
         
        
@@ -41,8 +43,7 @@ $id = $_SESSION['user_id'];
        }
 // Show whatever this activity is
 $current =  $user->selectGoals( $id);
-$goal = $user->getgoal($id);
-echo $goal;
+
 require('views/goals.php');
 require('views/footer.php');
 
