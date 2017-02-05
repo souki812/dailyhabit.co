45b86d8e285804dc0da5c9b97f580a014c4b1f66
Souki Alami <salam13@stlawu.edu>
2017-02-04 20:31:21 -0500

    Testing echo statement

--- goals.php
+++ goals.php
@@ -28,8 +28,7 @@ $id = $_SESSION['user_id'];
                 // Add a goal 
                 if ($_POST['task'] == 'goal') {
                     $success = $user->insert_goal( $_POST['goal'], $_POST['variable'], $_POST['unit'], $_POST['value'],  $id);
-                    $goal = $user->getgoal($id);
-                    echo $goal;
+                    echo $success;
                 }
         
        
@@ -43,6 +42,7 @@ $id = $_SESSION['user_id'];
        }
 // Show whatever this activity is
 $current =  $user->selectGoals( $id);
+$goal = $user->getgoal($id);
 
 require('views/goals.php');
 require('views/footer.php');
