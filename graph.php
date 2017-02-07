a285848fd7ecaebd54a53f0f2f4a4767de043703
Souki Alami <salam13@stlawu.edu>
2017-02-07 17:53:20 -0500

    Created new modal class graph.php to retrieve data for the graph

---
+++ graph.php
@@ -0,0 +1,39 @@
+<?php 
+session_start();
+
+
+require_once('models/database.php');
+$db = databaseConnection();
+
+
+
+// Must be logged in
+if (!isset($_SESSION['user_id'])) {
+    header('Location: ./');
+    exit();
+}
+
+$id = $_SESSION['user_id'];
+
+
+ if (!isset($db)) {
+        $_SESSION['message'] = "Could not connect to the database.";
+    }else{
+     
+      require_once('models/user.php');
+      $user = new User($db);
+
+      $goal = $user->getgoal($id);
+      echo $goal;
+        
+        
+       
+        }
+    
+       
+
+       
+
+
+
+
