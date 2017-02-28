daf805d1da0766b5cfdc03c389644d50473a659c
Souki Alami <salam13@stlawu.edu>
2017-02-28 09:11:11 -0500

    Fixed update method

---
+++ update.php
@@ -0,0 +1,48 @@
+<?php 
+session_start();
+
+
+require_once('models/database.php');
+$db = databaseConnection();
+
+
+
+// Must be logged in
+if (!isset($_SESSION['user_id'])) {
+    header('Location: ./');
+    exit();
+}
+
+$id = $_SESSION['user_id'];
+
+
+ if (!isset($db)) {
+        $_SESSION['message'] = "Could not connect to the database.";
+    }else{
+     
+      require_once('models/user.php');
+      $user = new User($db);
+
+   
+        
+        if (isset($_POST['task'])) {
+    
+                // Add a goal 
+                if ($_POST['task'] == 'goal') {
+                    $success = $user->insert_update( $_POST['variable'], $_POST['date'], $id, $_POST['goal_id']);
+                    
+                }
+        
+       
+        }
+    
+       
+
+       }
+// Show whatever this activity is
+$update =  $user->selectUpdate( $id);
+
+
+require('views/goals.php');
+require('views/footer.php');
+
