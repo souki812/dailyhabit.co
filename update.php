cb58d4f39856e37c4225e3ee6caaa943516ef527
Souki Alami <salam13@stlawu.edu>
2017-03-01 10:25:53 -0500

    Testing update.php

--- update.php
+++ update.php
@@ -30,7 +30,7 @@ $id = $_SESSION['user_id'];
                 // Add an update 
                 if ($_POST['task'] == 'update') {
                     $success = $user->insert_update( $_POST['variable'], $_POST['date'], $id, $_POST['goal_id']);
-                    
+                    echo $_POST['goal_id'];
                 }
         
        
