065a183372e53b6f72a50c286f89b33fe44e38ae
Souki Alami <salam13@stlawu.edu>
2017-03-13 11:45:28 -0400

    Added ajax call to get list of dates

---
+++ updatedate.php
@@ -0,0 +1,33 @@
+<?php
+
+session_start();
+require_once('models/database.php');
+$db = databaseConnection();
+
+
+
+// Must be logged in
+if (!isset($_SESSION['user_id'])) {
+    exit();
+}
+
+require_once('models/user.php');
+$user = new User($db);
+      
+$id = $_SESSION['user_id'];      
+
+
+
+ if (!isset($db)) {
+       exit();
+ }else{
+ 		
+ 		
+      //Getting all the dates clicked from the database in array format
+		$updatedates = $user->getupdatedate($id);
+
+		//PHP's associative array becomes an object literal 
+		echo json_encode($updatedates, JSON_PRETTY_PRINT);
+ }
+
+
