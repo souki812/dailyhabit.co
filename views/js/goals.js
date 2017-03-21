870a4578fbd3e811747294a2abda9cdc12945cff
Souki Alami <salam13@stlawu.edu>
2017-03-21 15:31:53 -0400

    changed width of bars

--- views/js/goals.js
+++ views/js/goals.js
@@ -110,12 +110,11 @@
                             datasets: [{
                               label: goal,
                               data: updatevalues,
+                              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                              borderColor: 'rgba(255,99,132,1)',
+                              borderWidth: 1
                             }],
 
-                            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                            borderColor: 'rgba(255,99,132,1)',
-                            borderWidth: 1
-        
                         },
                         options: {
                             title: {
