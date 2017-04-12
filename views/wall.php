ca4aaec32dd1eb2f7e53934e3d73a6828bd9f5a8
Souki Alami <salam13@stlawu.edu>
2017-04-12 15:07:50 -0400

    Created a wall tab

---
+++ views/wall.php
@@ -0,0 +1,26 @@
+   <!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DailyHabit</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    
+         
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+              
+            </div>
+
+     
+<?php require('menu.php'); ?>
+
+
+    
+    
+    
+                
+        </div>
+  
