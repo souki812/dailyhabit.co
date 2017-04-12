305d7c8ebd1c8fab62c589d6ddf572db0c1b4e58
Souki Alami <salam13@stlawu.edu>
2017-04-12 15:28:59 -0400

    Created a wall tab

--- views/wall.php
+++ views/wall.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>DailyHabit</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel="stylesheet" href="views/css/activity.css">
          <style type="text/css">
 
 
@@ -14,11 +15,6 @@
                     border-bottom: 20px solid #000;
                     font-family: Rockwell, serif;
                 }
-
-
-
-
-
 
         </style>
          
@@ -42,6 +38,100 @@
                 </center>
             </div>
         </div>
+
+
+
+
+
+        
+
+
+
+
+<div class="modal" id="modal2">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body">
+                <form action="wall.php" method="post" class="well">
+                 <form class="form">
+                    <label >Comment</label>
+                    <textarea class="form-control" rows="5" id="comment"   name="newsfeed" autocomplete="off" autofocus>
+                       
+                    </textarea>
+                    <input type="hidden" name="task" value="newsfeed">
+                    <button type="submit" class="btn btn-default add">Add</button>
+                 </form>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container page">
+    
+ <div class="row">
+    <button  class="btn btn-default secondbutton" data-toggle="modal" data-target="#modal2" >Post a comment</button>
+   <?php foreach ($comments as $row): ?>
+        <div class="col-md-9 col-md-offset-2  comment" >
+            <form action="wall.php" method="post" >
+                <div class="form-group">
+                    <img src="/views/uploads/<?php echo htmlentities($row['picture'], ENT_QUOTES, 'utf-8'); ?>"  class="img-circle" width="70" height="70"  onerror="if (this.src != 'views/images/no_photo.png') this.src = 'views/images/no_photo.png';">
+                    <label>
+                    <h4 class="text"><?php echo htmlentities($row['first'], ENT_QUOTES, 'utf-8'); ?>  <?php echo htmlentities($row['last'], ENT_QUOTES, 'utf-8'); ?></h4>
+                    <h4 class="text"><?php echo htmlentities($row['time'], ENT_QUOTES, 'utf-8'); ?></h4>
+                    </label>
+                </div>
+                
+            <h5 class="words"><?php echo htmlentities($row['comment'], ENT_QUOTES, 'utf-8'); ?></h5>
+            <input type="hidden" name="comment_id" value="<?php echo $row['comment_id']; ?>">
+            <input type="submit" name="delete" class="delete" value="delete">
+            </form>
+
+
+        </div>
+            <?php endforeach; ?>
+            
+ </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
     
     
--- wall.php
+++ wall.php
@@ -22,9 +22,45 @@ $id = $_SESSION['user_id'];
      
       require_once('models/user.php');
       $user = new User($db);
+
+
+      //Add a comment
+            if ($_POST['task'] == 'newsfeed') {
+                $success = $user->newsfeed( $_POST['newsfeed'], $id);
+            }
+       
+        
+        if (isset($_POST['comment_id'])) {
+            //Remove comment
+            $success= $user->remove_comment( $_POST['comment_id']);
+        }
+        
+        if(isset($_POST['profile']))
+            $allow = array("jpg", "jpeg", "gif", "png");
+            $todir = 'views/uploads/';
+            
+            
+    if ( $_FILES['file']['tmp_name'] ){
+    
+    $info = explode('.', strtolower( $_FILES['file']['name']) ); // whats the extension of the file
+
+    if ( in_array( end($info), $allow) ) {
+        if ( move_uploaded_file( $_FILES['file']['tmp_name'], $todir . basename($_FILES['file']['name']))){
+            //Save picture name in database
+            $success = $user->picture( $_FILES["file"]["name"], $id);
+            echo "File: " . $_FILES["file"]["error"] . "<br>";
+        }
+    }else{
+        // error this file ext is not allowed
+         echo "Error: " . $_FILES["file"]["error"] . "<br>";
+    }
         
         
         }
+
+         }
+
+
 
 // Show whatever this activity is
 
