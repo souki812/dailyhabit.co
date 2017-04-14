d7b125e25d165b961de70f87324cca182ff4091e
Souki Alami <salam13@stlawu.edu>
2017-04-14 17:47:04 -0400

    Changing comment height

--- views/wall.php
+++ views/wall.php
@@ -153,12 +153,13 @@
                 <div class="form-group">
                     <img src="/views/uploads/<?php echo htmlentities($row['picture'], ENT_QUOTES, 'utf-8'); ?>"  class="img-circle" width="70" height="70"  onerror="if (this.src != 'views/images/no_photo.png') this.src = 'views/images/no_photo.png';">
                     <label>
-                    <h6 class="text"><?php echo htmlentities($row['first'], ENT_QUOTES, 'utf-8'); ?>  <?php echo htmlentities($row['last'], ENT_QUOTES, 'utf-8'); ?></h6>
+                    <h5 class="text"><?php echo htmlentities($row['first'], ENT_QUOTES, 'utf-8'); ?>  <?php echo htmlentities($row['last'], ENT_QUOTES, 'utf-8'); ?></h5>
                     <h6 class="text"><?php echo date(("F j, Y, g:i a"), strtotime($row['time'])); ?></h6>
+                    <h4 class="text"><?php echo htmlentities($row['comment'], ENT_QUOTES, 'utf-8'); ?></h4>
                     </label>
                 </div>
                 
-            <h5 class="words"><?php echo htmlentities($row['comment'], ENT_QUOTES, 'utf-8'); ?></h5>
+            
             <input type="hidden" name="comment_id" value="<?php echo $row['comment_id']; ?>">
             <input type="submit" name="delete" class="delete" value="delete">
             </form>
