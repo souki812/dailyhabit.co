c92b932de3a1cdd6c3511b38e35918aaabe82638
Souki Alami <salam13@stlawu.edu>
2017-04-14 18:08:25 -0400

    Changing comment height

--- views/wall.php
+++ views/wall.php
@@ -18,7 +18,7 @@
                  .comment{
         border: 1px solid grey;
         font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif;
-        height: 50%;
+        height: 100px;
        
     }
     
